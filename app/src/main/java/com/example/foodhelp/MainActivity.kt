c2e4e3cc01a9fd3f755d5c8e59d19c6f1b4518d0
rz-eizy <duartealessandroxd@gmail.com>
2025-10-01 20:45:55 -0300

    refactor: Eliminar imports que no se usan
    Se eliminan las líneas de importación innecesarias en los archivos afectados para limpiar el código y mejorar la legibilidad.

--- app/src/main/java/com/example/foodhelp/MainActivity.kt
+++ app/src/main/java/com/example/foodhelp/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.foodhelp.screens.Home
--- app/src/main/java/com/example/foodhelp/components/BotonDividido.kt
+++ app/src/main/java/com/example/foodhelp/components/BotonDividido.kt
@@ -1,7 +1,6 @@
 package com.example.foodhelp.components
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 
 class BotonDividido {
     @Composable
