89dd7928b4c330ab7497eb289083008f23cdb2ca
rz-eizy <duartealessandroxd@gmail.com>
2025-10-01 09:49:52 -0300

    feature: restructuracion de carpetas
    Se re ordenaron las carptepas para poder tener un manejo optimo de las clases que se requieran en el futuro, para la implementación del interfaz
    
    Se corrigio un error en "AndroidManifest.xml" en donde no se encontraba el llamado correcto del "MainActivity", para lo cual se modifico el nombre de la carpeta "com,example.MyAplication" a "com.example.foodhelp"

--- app/src/main/java/com/example/foodhelp/MainActivity.kt
+++ app/src/main/java/com/example/foodhelp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.foodhelp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,14 +8,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.foodhelp.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
