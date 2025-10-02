98d81c24a7bf377d4edd975763c6d7eaf961ceb6
rz-eizy <duartealessandroxd@gmail.com>
2025-10-02 00:50:06 -0300

    refactor: Implementar Scaffold en HomePage
    Reemplazar el Column por Scaffold para estandarizar el layout de la pantalla y gestionar correctamente el padding del contenido principal (innerPadding), siguiendo las prácticas estándar de Jetpack Compose.

--- app/src/main/java/com/example/foodhelp/screens/Home.kt
+++ app/src/main/java/com/example/foodhelp/screens/Home.kt
@@ -6,41 +6,53 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.foodhelp.components.MySearchBar
+import com.example.foodhelp.ui.theme.ComponentAccent
 import com.example.foodhelp.ui.theme.MyApplicationTheme
+import com.example.foodhelp.ui.theme.SurfaceBackground
 
 @Composable
 fun HomePage(){
-
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFFD2D2D2)),
-        verticalArrangement = Arrangement.SpaceBetween
-    ){
-        MySearchBar(
-            onSearch = {}, // Aqui implementar la navegacion entre pantallas
-            modifier = Modifier.fillMaxWidth()
-        )
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth()
-        ) {
-            // Lo que va al medio
+    Scaffold(
+        topBar = {
+            MySearchBar(
+                onSearch = {}, // Aqui implementar la navegacion entre pantallas
+                modifier = Modifier.fillMaxWidth().background(SurfaceBackground)
+            )
+        },
+        bottomBar = {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                //Agregar aqui el boton segmentado
+            }
         }
-        Box(
-            modifier = Modifier.fillMaxWidth(),
-            contentAlignment = Alignment.Center
-        ) {
-            //Agregar aqui el boton segmentado
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(SurfaceBackground),
+        ){
+            Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                //Agregar aqui las categorias
+            }
         }
     }
+
 }
 
 @Preview(showBackground = true)
