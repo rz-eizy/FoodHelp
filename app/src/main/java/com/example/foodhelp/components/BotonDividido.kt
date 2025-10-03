043aed24b40215dd87feef44dec9a943d4204502
rz-eizy <duartealessandroxd@gmail.com>
2025-10-03 02:30:50 -0300

    feat: Agregar función SaveAndIngredients
    Implmentacion del codigo de la funcion, el cual permite mediante un row tener 2 botones, cada uno con realizando distintas funciones mediante a sus onclick.

--- app/src/main/java/com/example/foodhelp/components/BotonDividido.kt
+++ app/src/main/java/com/example/foodhelp/components/BotonDividido.kt
@@ -1,8 +1,47 @@
 package com.example.foodhelp.components
 
+import android.widget.Button
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 
 @Composable
 fun BotonSegmentado(){
 
+}
+
+@Composable
+fun SaveAndIngredients(
+    onIngredientsClick: ()-> Unit,
+    onSaveClick: ()-> Unit
+){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+       Button(
+           onClick = onIngredientsClick,
+           modifier = Modifier
+               .weight(1f)
+               .padding(end = 8.dp),
+           shape = RoundedCornerShape(20.dp)
+       ) { Text("Ingredientes") }
+
+        Button(
+            onClick = onSaveClick,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 8.dp),
+            shape = RoundedCornerShape(20.dp)
+        ) { Text("Guardar")}
+    }
 }
