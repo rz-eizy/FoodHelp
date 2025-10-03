4f3c4d47093a3716212eb263b71077fce5112d2d
rz-eizy <duartealessandroxd@gmail.com>
2025-10-02 23:12:34 -0300

    feat: Implementación de pantalla RecipePage
    Implementa el Composable RecipePage usando Scaffold como estructura base. Esto establece las secciones de la barra superior, inferior y contenido principal, mejorando la legibilidad y modularidad.

--- app/src/main/java/com/example/foodhelp/screens/Recipe.kt
+++ app/src/main/java/com/example/foodhelp/screens/Recipe.kt
@@ -1,5 +1,45 @@
 package com.example.foodhelp.screens
 
-class Recipe {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.foodhelp.ui.theme.MyApplicationTheme
+import com.example.foodhelp.ui.theme.SurfaceBackground
 
+@Composable
+fun RecipePage(){
+    Scaffold(
+        topBar = {/*    Contenido Superior  */},
+        bottomBar = {/* Contenido Inferior   */}
+
+    ) { innerPadding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .background(SurfaceBackground),
+            ){Box(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){/*    Contenido Central   */}
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRecipe(){
+    MyApplicationTheme { 
+        RecipePage()
+    }
 }
