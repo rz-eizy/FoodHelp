997981b1b14bd2e7bec51653971bcf000e942533
rz-eizy <duartealessandroxd@gmail.com>
2025-10-17 20:58:31 -0300

    feat(ui): Completa la estructura Scaffold, BottomBar y contenido
    Se completó la estructura principal (Scaffold) de la interfaz de usuario integrando los siguientes componentes:
    
    Se integró el componente `BottomBar`, incluyendo la implementación de `MySegmentedButton` para la navegación.
    Se integró la `HomeCard` que muestra las categorías en el área central del `Scaffold`.
    
    Se configuraron los `Modifiers` en `TopBar` y `BottomBar` para gestionar el espaciado y asegurar que el contenido no quede oculto por los componentes nativos del sistema.

--- app/src/main/java/com/example/foodhelp/screens/Home.kt
+++ app/src/main/java/com/example/foodhelp/screens/Home.kt
@@ -4,16 +4,26 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.foodhelp.components.HomeCard
 import com.example.foodhelp.components.MySearchBar
+import com.example.foodhelp.components.MySegmentedButton
 import com.example.foodhelp.ui.theme.ComponentAccent
 import com.example.foodhelp.ui.theme.MyApplicationTheme
 import com.example.foodhelp.ui.theme.SurfaceBackground
@@ -24,15 +34,24 @@ fun HomePage(){
         topBar = {
             MySearchBar(
                 onSearch = {}, // Aqui implementar la navegacion entre pantallas
-                modifier = Modifier.fillMaxWidth().background(SurfaceBackground)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(SurfaceBackground)
+                    .windowInsetsPadding(WindowInsets.statusBars)
             )
         },
         bottomBar = {
             Box(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(SurfaceBackground),
                 contentAlignment = Alignment.Center
             ) {
-                //Agregar aqui el boton segmentado
+                MySegmentedButton(
+                    modifier = Modifier
+                        .background(SurfaceBackground)
+                        .windowInsetsPadding(WindowInsets.navigationBars)
+                )
             }
         }
     ) { innerPadding ->
@@ -48,7 +67,10 @@ fun HomePage(){
                     .fillMaxWidth(),
                 contentAlignment = Alignment.Center
             ){
-                //Agregar aqui las categorias
+                HomeCard(
+                    modifier = Modifier
+                        .size(height = 550.dp, width = 325.dp)
+                )
             }
         }
     }
