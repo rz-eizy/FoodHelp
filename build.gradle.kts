12894b899665e14c55a951c4b9f37f62441f0f6b
BastianWenckhans <basty.wenckhansbello@gmail.com>
2025-10-26 19:46:40 -0300

    fix(sonar): Corrige el analisis para proyecto multi-modulo
    El analisis de Sonar estaba configurado incorrectamente y solo detectaba el modulo 'backend', ignorando el modulo 'app'.
    
    Se centralizo la configuracion de Sonar en el archivo build.gradle.kts (FoodHelp) y se eliminaron las configuraciones conflictivas de los modulos . Esto permite que Sonar analice el proyecto completo (app y backend) correctamente.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,15 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     // SonarQube
-    id("org.sonarqube") version "6.3.1.5724" apply false
+    id("org.sonarqube") version "6.3.1.5724"
+}
+
+sonar {
+    properties {
+
+        property("sonar.projectKey", "FoodHelp")
+        property("sonar.projectName", "FoodHelp")
+        property("sonar.host.url", "http://localhost:9000")
+        property("sonar.token", "sqp_b164150551bfc6e1f925b209e10f325ea0c612c3")
+    }
 }
